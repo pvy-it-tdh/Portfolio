81eae9adb70e7c5bdf64d20381b3a70b28a0c137
pvy-it-tdh <phucvy107@gmail.com>
2024-04-23 14:03:18 +0700

    update Nav

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,22 +3,24 @@ import { Link } from "react-scroll";
 const Nav = () => {
   const content = (
     <>
-      <div className="">
-        <Link to="Home">
-          <li>Home</li>
-        </Link>
-        <Link to="About">
-          <li>About</li>
-        </Link>
-        <Link to="Services">
-          <li>Services</li>
-        </Link>
-        <Link to="Projects">
-          <li>Projects</li>
-        </Link>
-        <Link to="Contact">
-          <li>Contact</li>
-        </Link>
+      <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate 900 trasition">
+        <ul className="text-center text-xl p-20">
+          <Link spy={true} smooth={true}  to="Home">
+            <li className="my-4 borderb border-slate-800 hover:bg-slate-800 hover:rounded">Home</li>
+          </Link>
+          <Link spy={true} smooth={true} to="About">
+            <li className="my-4 borderb border-slate-800 hover:bg-slate-800 hover:rounded">About</li>
+          </Link>
+          <Link spy={true} smooth={true} to="Services">
+            <li className="my-4 borderb border-slate-800 hover:bg-slate-800 hover:rounded">Services</li>
+          </Link>
+          <Link spy={true} smooth={true} to="Projects">
+            <li className="my-4 borderb border-slate-800 hover:bg-slate-800 hover:rounded">Projects</li>
+          </Link>
+          <Link spy={true} smooth={true} to="Contact">
+            <li className="my-4 borderb border-slate-800 hover:bg-slate-800 hover:rounded">Contact</li>
+          </Link>
+        </ul>
       </div>
     </>
   );
@@ -31,20 +33,20 @@ const Nav = () => {
         <div className="lg: flex md: lg: flex-1 items center justify-end font-normal">
           <div className="flex-10">
             <ul className="flex gap-8 mr-16 text-[18px] ">
-              <Link to="Home">
-                <li>Home</li>
+              <Link spy={true} smooth={true} to="Home">
+                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-950 hover:border-fuchsia-600 cursor-pointer">Home</li>
               </Link>
-              <Link to="About">
-                <li>About</li>
+              <Link spy={true} smooth={true} to="About">
+                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-950 hover:border-fuchsia-600 cursor-pointer">About</li>
               </Link>
-              <Link to="Services">
-                <li>Services</li>
+              <Link spy={true} smooth={true} to="Services">
+                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-950 hover:border-fuchsia-600 cursor-pointer">Services</li>
               </Link>
-              <Link to="Projects">
-                <li>Projects</li>
+              <Link spy={true} smooth={true} to="Projects"> 
+                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-950 hover:border-fuchsia-600 cursor-pointer">Projects</li>
               </Link>
-              <Link to="Contact">
-                <li>Contact</li>
+              <Link spy={true} smooth={true}k to="Contact">
+                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-950 hover:border-fuchsia-600 cursor-pointer">Contact</li>
               </Link>
             </ul>
           </div>
