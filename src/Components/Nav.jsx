682f796129562bb4478d10cfa0491739ea519b4f
pvy-it-tdh <phucvy107@gmail.com>
2024-04-23 14:37:32 +0700

    complete Nav

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,57 +1,83 @@
+import { useState } from "react";
 import { Link } from "react-scroll";
+import { FaTimes, FaBars } from "react-icons/fa";
 
 const Nav = () => {
-  const content = (
-    <>
-      <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate 900 trasition">
-        <ul className="text-center text-xl p-20">
-          <Link spy={true} smooth={true}  to="Home">
-            <li className="my-4 borderb border-slate-800 hover:bg-slate-800 hover:rounded">Home</li>
-          </Link>
-          <Link spy={true} smooth={true} to="About">
-            <li className="my-4 borderb border-slate-800 hover:bg-slate-800 hover:rounded">About</li>
-          </Link>
-          <Link spy={true} smooth={true} to="Services">
-            <li className="my-4 borderb border-slate-800 hover:bg-slate-800 hover:rounded">Services</li>
-          </Link>
-          <Link spy={true} smooth={true} to="Projects">
-            <li className="my-4 borderb border-slate-800 hover:bg-slate-800 hover:rounded">Projects</li>
-          </Link>
-          <Link spy={true} smooth={true} to="Contact">
-            <li className="my-4 borderb border-slate-800 hover:bg-slate-800 hover:rounded">Contact</li>
-          </Link>
-        </ul>
-      </div>
-    </>
-  );
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
   return (
     <nav>
       <div className="h-10vh flex justify-between z-50 text-white lg:py-5 px-20 py-4">
         <div className="flex items-center flex-1">
-          <span className="text-3x1 font-bold">Logo</span>
+          <span className="text-3xl font-bold">Logo</span>
         </div>
-        <div className="lg: flex md: lg: flex-1 items center justify-end font-normal">
-          <div className="flex-10">
-            <ul className="flex gap-8 mr-16 text-[18px] ">
-              <Link spy={true} smooth={true} to="Home">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-950 hover:border-fuchsia-600 cursor-pointer">Home</li>
-              </Link>
-              <Link spy={true} smooth={true} to="About">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-950 hover:border-fuchsia-600 cursor-pointer">About</li>
-              </Link>
-              <Link spy={true} smooth={true} to="Services">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-950 hover:border-fuchsia-600 cursor-pointer">Services</li>
-              </Link>
-              <Link spy={true} smooth={true} to="Projects"> 
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-950 hover:border-fuchsia-600 cursor-pointer">Projects</li>
-              </Link>
-              <Link spy={true} smooth={true}k to="Contact">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-950 hover:border-fuchsia-600 cursor-pointer">Contact</li>
-              </Link>
-            </ul>
-          </div>
+        <div className="hidden lg:flex lg:items-center lg:flex-1 lg:justify-end lg:font-normal">
+          <ul className="flex gap-8 mr-16 text-base">
+            <Link spy={true} smooth={true} to="Home">
+              <li className="hover:text-fuchsia-600 transition cursor-pointer">
+                Home
+              </li>
+            </Link>
+            <Link spy={true} smooth={true} to="About">
+              <li className="hover:text-fuchsia-600 transition cursor-pointer">
+                About
+              </li>
+            </Link>
+            <Link spy={true} smooth={true} to="Services">
+              <li className="hover:text-fuchsia-600 transition cursor-pointer">
+                Services
+              </li>
+            </Link>
+            <Link spy={true} smooth={true} to="Projects">
+              <li className="hover:text-fuchsia-600 transition cursor-pointer">
+                Projects
+              </li>
+            </Link>
+            <Link spy={true} smooth={true} to="Contact">
+              <li className="hover:text-fuchsia-600 transition cursor-pointer">
+                Contact
+              </li>
+            </Link>
+          </ul>
+        </div>
+        <div className="lg:hidden">
+          <button className="transition" onClick={handleClick}>
+            {click ? <FaTimes /> : <FaBars />}
+          </button>
         </div>
       </div>
+      {click && (
+        <div className="lg:hidden absolute top-16 w-full left-0 right-0 bg-slate-200 transition">
+          <ul className="text-center text-xl p-20">
+            <Link spy={true} smooth={true} to="Home">
+              <li className="my-4 border-b border-slate-800 hover:bg-slate-600 hover:rounded">
+                Home
+              </li>
+            </Link>
+            <Link spy={true} smooth={true} to="About">
+              <li className="my-4 border-b border-slate-800 hover:bg-slate-600 hover:rounded">
+                About
+              </li>
+            </Link>
+            <Link spy={true} smooth={true} to="Services">
+              <li className="my-4 border-b border-slate-800 hover:bg-slate-600 hover:rounded">
+                Services
+              </li>
+            </Link>
+            <Link spy={true} smooth={true} to="Projects">
+              <li className="my-4 border-b border-slate-800 hover:bg-slate-600 hover:rounded">
+                Projects
+              </li>
+            </Link>
+            <Link spy={true} smooth={true} to="Contact">
+              <li className="my-4 border-b border-slate-800 hover:bg-slate-600 hover:rounded">
+                Contact
+              </li>
+            </Link>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
